ad624913c938438ee72bf912584178a5f08cac5f
cheng cui <cheng.cui@etocrm.com>
2018-08-15 18:55:24 +0800

    订单管理后台逻辑处理，分页还有问题！

---
+++ admin/Xn.Platform.Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Xn.Platform.Domain.Impl.Order;
+using Xn.Platform.Domain.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Xn.Platform.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private OrderMainService orderMainService = new OrderMainService();
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 主订单列表
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult MainOrder(int pageIndex = 1, int pageSize = 10)
+        {
+            var result = orderMainService.PageList(new OrderMainRequest.PageRequest
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize
+            });
+            
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- domain/Xn.Platform.Domain.Impl/Order/OrderMainService.cs
+++ domain/Xn.Platform.Domain.Impl/Order/OrderMainService.cs
@@ -1,13 +1,39 @@
-﻿using System;
+﻿using Xn.Platform.Domain.Order;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xn.Platform.Abstractions.Domain;
+using Xn.Platform.Data.MySql.Order;
 
-namespace Plu.Platform.Domain.Impl.Order
+namespace Xn.Platform.Domain.Impl.Order
 {
+
     public class OrderMainService
     {
+        private OrderMainRepository tourUserRepository = new OrderMainRepository();
+        public ResultWithCodeEntity<PagedEntity<OrderMainResponse.PageResponse>> PageList(OrderMainRequest.PageRequest request)
+        {
+            try
+            {
+                return new ResultWithCodeEntity<PagedEntity<OrderMainResponse.PageResponse>>
+                {
+                    Code = ResultCode.Success,
+                    Data = tourUserRepository.PageInfo(request)
+                };
+            }
+            catch (Exception ex)
+            {
+
+                return new ResultWithCodeEntity<PagedEntity<OrderMainResponse.PageResponse>>
+                {
+                    Code = ResultCode.ExceptionError,
+                    Data = new PagedEntity<OrderMainResponse.PageResponse>()
+                };
+            }
+
+        }
 
     }
 }
